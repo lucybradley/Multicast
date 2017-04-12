3d712cc4a303e8d2c9148a733793024bde627365
lucybradley <lucy.bradley25@uga.edu>
2017-04-12 18:09:10 -0400

    Coordinator stuff

--- src/CThread.java
+++ src/CThread.java
@@ -1,4 +1,19 @@
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 
 public class CThread extends Thread{
+	
+	private Socket sock;
+	private OutputStream writer;
+	private InputStream reader;
+	
+	public CThread(Socket sock){
+		this.sock = sock;
+	}
 
+	@Override
+	public void run(){
+		
+	}
 }
--- src/Coordinator.java
+++ src/Coordinator.java
@@ -1,4 +1,11 @@
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Coordinator {
-
+	
+	public static HashMap<Integer, Boolean> clientStatus;
+	public static HashMap<Integer, Integer> clientPort;
+	public static HashMap<Integer, InetAddress> clientIP;
+	public static HashMap<Integer, ArrayList<String>> saveMsgs;
 }
