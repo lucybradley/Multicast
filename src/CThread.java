99f82f061f1354b7f4ab793421dab7e4faae2e76
lucybradley <lucy.bradley25@uga.edu>
2017-04-12 18:56:19 -0400

    CThread stuff

--- src/CThread.java
+++ src/CThread.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
@@ -5,8 +6,8 @@ import java.net.Socket;
 public class CThread extends Thread{
 	
 	private Socket sock;
-	private OutputStream writer;
-	private InputStream reader;
+	private OutputStream writer = null;
+	private InputStream reader = null;
 	
 	public CThread(Socket sock){
 		this.sock = sock;
@@ -14,6 +15,36 @@ public class CThread extends Thread{
 
 	@Override
 	public void run(){
-		//run
+		try {
+			writer = sock.getOutputStream();
+			reader = sock.getInputStream();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+		
+		byte[] buf = new byte[1024];
+		int readIn = 0;
+		try {
+			readIn = reader.read(buf);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		String command = new String(buf, 0, readIn);
+		try {
+			writer.write("OK".getBytes());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		parseInput(command);
+	}
+	
+	public void parseInput(String command){
+		
+	}
+	
+	public void deregister(int id){
+		
 	}
 }
