330341bb4771d3fce759ed84373cec7b6e42cf79
lucybradley <lucy.bradley25@uga.edu>
2017-04-12 19:03:17 -0400

    Cthread empty methods

--- src/CThread.java
+++ src/CThread.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.InetAddress;
 import java.net.Socket;
 
 public class CThread extends Thread{
@@ -44,7 +45,23 @@ public class CThread extends Thread{
 		
 	}
 	
+	public void register(int id, InetAddress addr, int port){
+		
+	}
+	
 	public void deregister(int id){
+		
+	}
+	
+	public void disconnect(int id){
+		
+	}
+	
+	public void reconnect(int id){
+		
+	}
+	
+	public void msend(String msg){
 		
 	}
 }
